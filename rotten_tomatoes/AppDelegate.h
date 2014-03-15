b73fbdfba958dd0a7994f8c89ae32960cea90d21
Arpan Nanavati <nanavatiarpan@gmail.com>
2014-03-14 20:21:45 -0700

    Initial Commit

---
+++ rotten_tomatoes/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  rotten_tomatoes
+//
+//  Created by Arpan Nanavati on 3/14/14.
+//  Copyright (c) 2014 Arpan Nanavati. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ rotten_tomatoesTests/rotten_tomatoesTests.m
@@ -0,0 +1,34 @@
+//
+//  rotten_tomatoesTests.m
+//  rotten_tomatoesTests
+//
+//  Created by Arpan Nanavati on 3/14/14.
+//  Copyright (c) 2014 Arpan Nanavati. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface rotten_tomatoesTests : XCTestCase
+
+@end
+
+@implementation rotten_tomatoesTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
