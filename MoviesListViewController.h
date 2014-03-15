f388d96007e748c9fad3034d1ce55852d5a203ff
Arpan Nanavati <nanavatiarpan@gmail.com>
2014-03-15 13:20:23 -0700

    include AFnetworking lib

--- MoviesListViewController.h
+++ MoviesListViewController.h
@@ -8,6 +8,7 @@
 
 #import <UIKit/UIKit.h>
 #import "MovieCell.h"
+#import "UIImageView+AFNetworking.h"
 
 @interface MoviesListViewController : UIViewController <UITableViewDataSource, UITableViewDelegate>
 
